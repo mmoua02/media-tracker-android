676765b6cecae2856f822364d1d524dc4dc0fab5
Mai Moua <ca6120mg@go.minnstate.edu>
2026-05-21 20:53:04 -0500

    Updated the app theme.

--- app/src/main/java/edu/metrostate/ics342/mediatracker/ui/auth/LoginScreen.kt
+++ app/src/main/java/edu/metrostate/ics342/mediatracker/ui/auth/LoginScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -122,3 +123,4 @@ fun LoginScreen(
         }
     }
 }
+
